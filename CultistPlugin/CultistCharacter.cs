b8477bdca9a4dcfef7011b7229b7dc330e87c758
Markus Feng <markusfeng@gmail.com>
2021-07-09 00:20:04 -0700

    Changed instances of Henry to Cultist, merged plugins

---
+++ CultistPlugin/CultistCharacter.cs
@@ -0,0 +1,64 @@
+﻿using BepInEx;
+using CultistPlugin.Modules.Survivors;
+using R2API.Utils;
+using RoR2;
+using System.Collections.Generic;
+using System.Security;
+using System.Security.Permissions;
+
+namespace CultistPlugin
+{
+
+    public class CultistCharacter
+    {
+        internal List<SurvivorBase> Survivors = new List<SurvivorBase>();
+
+        public void Awake()
+        {
+            // load assets and read config
+            Modules.Assets.Initialize();
+            Modules.Config.ReadConfig();
+            Modules.States.RegisterStates(); // register states for networking
+            Modules.Buffs.RegisterBuffs(); // add and register custom buffs/debuffs
+            Modules.Projectiles.RegisterProjectiles(); // add and register custom projectiles
+            Modules.Tokens.AddTokens(); // register name tokens
+            Modules.ItemDisplays.PopulateDisplays(); // collect item display prefabs for use in our display rules
+
+            // survivor initialization
+            new MyCharacter().Initialize();
+
+            // now make a content pack and add it- this part will change with the next update
+            new Modules.ContentPacks().Initialize();
+
+            RoR2.ContentManagement.ContentManager.onContentPacksAssigned += LateSetup;
+
+            Hook();
+        }
+
+        private void LateSetup(HG.ReadOnlyArray<RoR2.ContentManagement.ReadOnlyContentPack> obj)
+        {
+            // have to set item displays later now because they require direct object references..
+            Modules.Survivors.MyCharacter.instance.SetItemDisplays();
+        }
+
+        private void Hook()
+        {
+            // run hooks here, disabling one is as simple as commenting out the line
+            On.RoR2.CharacterBody.RecalculateStats += CharacterBody_RecalculateStats;
+        }
+
+        private void CharacterBody_RecalculateStats(On.RoR2.CharacterBody.orig_RecalculateStats orig, CharacterBody self)
+        {
+            orig(self);
+
+            // a simple stat hook, adds armor after stats are recalculated
+            if (self)
+            {
+                if (self.HasBuff(Modules.Buffs.armorBuff))
+                {
+                    self.armor += 300f;
+                }
+            }
+        }
+    }
+}
---
+++ CultistPlugin/Modules/States.cs
@@ -0,0 +1,24 @@
+﻿using CultistPlugin.SkillStates;
+using CultistPlugin.SkillStates.BaseStates;
+using System.Collections.Generic;
+using System;
+
+namespace CultistPlugin.Modules
+{
+    public static class States
+    {
+        internal static List<Type> entityStates = new List<Type>();
+
+        internal static void RegisterStates()
+        {
+            entityStates.Add(typeof(BaseMeleeAttack));
+            entityStates.Add(typeof(SlashCombo));
+
+            entityStates.Add(typeof(Shoot));
+
+            entityStates.Add(typeof(Roll));
+
+            entityStates.Add(typeof(ThrowBomb));
+        }
+    }
+}
